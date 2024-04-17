ee010f93a27dea843779b345816af7cfba540dba
anilmadala <44867303+anilmadala@users.noreply.github.com>
2024-04-17 17:49:09 +0530

    Update trigger_workflow.sh

--- trigger_workflow.sh
+++ trigger_workflow.sh
@@ -9,4 +9,4 @@ curl -X POST \
   -u "${REPO_OWNER}:${TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
   "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/${WORKFLOW_NAME}/dispatches" \
-  -d '{"ref":"main"}'
+  -d '{"ref":"master"}'
