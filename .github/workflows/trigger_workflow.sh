d9b2228651df83bc4c49d181cc15a347fec2d340
anilmadala <44867303+anilmadala@users.noreply.github.com>
2024-04-17 15:52:35 +0530

    Update trigger_workflow.sh

--- .github/workflows/trigger_workflow.sh
+++ .github/workflows/trigger_workflow.sh
@@ -3,7 +3,7 @@
 REPO_OWNER="anilmadala94@gmail.com"
 REPO_NAME="injurymapping"
 WORKFLOW_NAME="reusableworkflow.yml"
-TOKEN="your_personal_access_token"
+TOKEN="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"
  
 curl -X POST \
   -u "${REPO_OWNER}:${TOKEN}" \
