cf08c3dfb796e6e77cc78f88adad798bed0e5c92
anilmadala <44867303+anilmadala@users.noreply.github.com>
2024-04-18 12:01:03 +0530

    Update trigger_workflow.sh

--- trigger_workflow.sh
+++ trigger_workflow.sh
@@ -5,7 +5,7 @@ REPO_NAME="injurymapping"
 QS_WORKFLOW_NAME="QS.yml"
 TEST_WORKFLOW_NAME="reusableworkflow.yml"
 
-if [ ${{ github.event.inputs.environment }} = 'dev' ];
+if [ "${github.event.inputs.environment}" = 'dev' ];
 then
 curl -X POST \
   -u "${REPO_OWNER}:${TOKEN}" \
