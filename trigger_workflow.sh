0cf97d1a6a1971f0316b3fa6cd89ae2293566f46
anilmadala <44867303+anilmadala@users.noreply.github.com>
2024-04-18 11:51:10 +0530

    Update trigger_workflow.sh

--- trigger_workflow.sh
+++ trigger_workflow.sh
@@ -5,13 +5,15 @@ REPO_NAME="injurymapping"
 QS_WORKFLOW_NAME="QS.yml"
 TEST_WORKFLOW_NAME="reusableworkflow.yml"
 
-if [ $env == 'dev' ]; then
+if [ $env = 'dev' ]
+then
 curl -X POST \
   -u "${REPO_OWNER}:${TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
   "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/actions/workflows/${QS_WORKFLOW_NAME}/dispatches" \
   -d '{"ref":"master"}'
-  elif [ $env == 'staging' ]; then
+  elif [ $env = 'staging' ]
+ then
   curl -X POST \
   -u "${REPO_OWNER}:${TOKEN}" \
   -H "Accept: application/vnd.github.v3+json" \
